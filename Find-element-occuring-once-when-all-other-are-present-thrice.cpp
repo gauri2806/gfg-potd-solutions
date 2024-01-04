ccb68d3db6748ad5ecc0c4d7c880e4afdc1e0ece
Gauri Katti <93340535+gauri2806@users.noreply.github.com>
2024-01-04 14:55:23 +0530

    4th Jan,2024 GFG POTD solution

---
+++ Find-element-occuring-once-when-all-other-are-present-thrice.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int singleElement(int arr[] ,int N) {
+        // code here
+        map<int, int> mp;
+        for(int i=0;i<N;++i) mp[arr[i]]++;
+        for(auto i:mp){
+            if(i.second==3) continue;
+            else return i.first;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int arr[N];
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout<<ob.singleElement(arr,N);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

0b5d6549b55f23067fb277cd15f9c246800321b4
Gauri Katti <gaurikatti2806@gmail.com>
2024-01-12 16:23:56 +0530

    12th Jan,2024 GFG POTD solution

---
+++ Reverse-First-K-elements-of-Queue.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+    public:
+    
+    // Function to reverse first k elements of a queue.
+    queue<int> modifyQueue(queue<int> q, int k) {
+        stack<int> st;
+        int n=q.size();
+        while(k--){
+            st.push(q.front());
+            q.pop();
+            n--;
+        }
+        while(!st.empty()){
+            q.push(st.top());
+            st.pop();
+        }
+        while(n--){
+            int x=q.front();
+            q.pop();
+            q.push(x);
+        }
+        return q;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n, k;
+        cin >> n >> k;
+        queue<int> q;
+        while (n-- > 0) {
+            int a;
+            cin >> a;
+            q.push(a);
+        }
+        Solution ob;
+        queue<int> ans = ob.modifyQueue(q, k);
+        while (!ans.empty()) {
+            int a = ans.front();
+            ans.pop();
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends

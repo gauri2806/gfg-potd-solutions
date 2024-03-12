ce57dcb6e519082213c0d369ac14870d69806f7f
Gauri Katti <gaurikatti2806@gmail.com>
2024-03-12 13:53:49 +0530

    12th march 2024, GFG POTD Solution

---
+++ march-2024/Generalised-Fibonacci-numbers.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+private:
+    vector<vector<long long>> mat;
+    vector<vector<long long>> res;
+    
+    void multiply(vector<vector<long long>>& res, const vector<vector<long long>>& mat, long long m) {
+        vector<vector<long long>> res1(3, vector<long long>(3, 0));
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                for (int k = 0; k < 3; ++k) {
+                    res1[i][j] += (res[i][k] * mat[k][j]) % m;
+                    res1[i][j] %= m;
+                }
+            }
+        }
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                res[i][j] = res1[i][j];
+            }
+        }
+    }
+    
+    void matrixExponentiation(long long n, long long m) {
+        while (n > 0) {
+            if (n & 1) {
+                multiply(res, mat, m);
+            }
+            multiply(mat, mat, m);
+            n /= 2;
+        }
+    }
+    
+public:
+    long long genFibNum(long long a, long long b, long long c, long long n, long long m) {
+        res = vector<vector<long long>>(3, vector<long long>(3, 0));
+        res[0][0] = res[1][1] = res[2][2] = 1;
+        
+        mat = vector<vector<long long>>(3, vector<long long>(3, 0));
+        mat[0][0] = a;
+        mat[0][1] = b;
+        mat[0][2] = mat[1][0] = mat[2][2] = 1;
+        mat[1][1] = mat[1][2] = mat[2][0] = mat[2][1] = 0;
+        
+        if (n <= 2) {
+            return 1 % m;
+        } else {
+            matrixExponentiation(n - 2, m);
+            return (res[0][0] + res[0][1] + c * res[0][2]) % m;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long a,b,c,n,m;
+        
+        cin>>a>>b>>c>>n>>m;
+
+        Solution ob;
+        cout << ob.genFibNum(a,b,c,n,m) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
